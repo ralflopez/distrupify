880fc5e8b727c613d5fe2076591f88d31edb20be
Ralf <ralflopez3@gmail.com>
2024-01-12 13:22:50 +0800

    :lipstick: style navbar

--- src/main/react/distrupify/src/App.tsx
+++ src/main/react/distrupify/src/App.tsx
@@ -1,7 +1,31 @@
-import { AppShell, Burger } from "@mantine/core";
+import {
+  AppShell,
+  Burger,
+  Button,
+  Divider,
+  ScrollArea,
+  Text,
+  rem,
+} from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
+import {
+  IconActivity,
+  IconAdjustments,
+  IconApps,
+  IconBox,
+  IconDashboard,
+  IconPackageImport,
+  IconReceipt,
+  IconReceipt2,
+  IconUserShare,
+} from "@tabler/icons-react";
 import { ReactNode } from "react";
-import { Link, RouterProvider, createBrowserRouter } from "react-router-dom";
+import {
+  RouterProvider,
+  createBrowserRouter,
+  useLocation,
+  useNavigate,
+} from "react-router-dom";
 import { InventoryAdjustment } from "./components/inventory-adjustment/InventoryAdjustment";
 import { Products } from "./components/products/Products";
 import { PurchaseOrder } from "./components/purchase-order/PurchaseOrder";
@@ -9,68 +33,102 @@ import { Sales } from "./components/sales/Sales";
 import { Suppliers } from "./components/suppliers/Suppliers";
 import { Transactions } from "./components/transactions/Transactions";
 
-const router = createBrowserRouter([
+const links_data = [
   {
-    path: "/",
-    element: (
-      <div>
-        <h1>Hello World</h1>
-        <Link to="about">About Us</Link>
-      </div>
-    ),
+    label: "Actions",
+    icon: IconActivity,
+    initiallyOpened: true,
+    links: [
+      {
+        label: "Stock Adjustment",
+        icon: IconAdjustments,
+        path: "/inventories/adjustments",
+        element: (
+          <Layout>
+            <InventoryAdjustment />
+          </Layout>
+        ),
+      },
+      {
+        label: "Sales",
+        icon: IconReceipt2,
+        path: "/sales",
+        element: (
+          <Layout>
+            <Sales />
+          </Layout>
+        ),
+      },
+      {
+        label: "Purchase Orders",
+        icon: IconPackageImport,
+        path: "/purchase-orders",
+        element: (
+          <Layout>
+            <PurchaseOrder />
+          </Layout>
+        ),
+      },
+    ],
   },
   {
-    path: "/inventories/adjustments",
-    element: (
-      <Layout>
-        <InventoryAdjustment />
-      </Layout>
-    ),
+    label: "Data",
+    icon: IconDashboard,
+    initiallyOpened: true,
+    links: [
+      {
+        label: "Transactions",
+        icon: IconReceipt,
+        path: "/transactions",
+        element: (
+          <Layout>
+            <Transactions />
+          </Layout>
+        ),
+      },
+    ],
   },
   {
-    path: "/transactions",
-    element: (
-      <Layout>
-        <Transactions />
-      </Layout>
-    ),
+    label: "Entities",
+    icon: IconApps,
+    initiallyOpened: true,
+    links: [
+      {
+        label: "Products",
+        icon: IconBox,
+        path: "/products",
+        element: (
+          <Layout>
+            <Products />
+          </Layout>
+        ),
+      },
+      {
+        label: "Suppliers",
+        icon: IconUserShare,
+        path: "/suppliers",
+        element: (
+          <Layout>
+            <Suppliers />
+          </Layout>
+        ),
+      },
+    ],
   },
-  {
-    path: "/products",
-    element: (
-      <Layout>
-        <Products />
-      </Layout>
-    ),
-  },
-  {
-    path: "/purchase-orders",
-    element: (
-      <Layout>
-        <PurchaseOrder />
-      </Layout>
-    ),
-  },
-  {
-    path: "/sales",
-    element: (
-      <Layout>
-        <Sales />
-      </Layout>
-    ),
-  },
-  {
-    path: "/suppliers",
-    element: (
-      <Layout>
-        <Suppliers />
-      </Layout>
-    ),
-  },
-]);
+];
+
+const router = createBrowserRouter(
+  links_data
+    .flatMap((l) => l.links)
+    .map(({ path, label, element }) => ({ path, label, element }))
+);
 
 function Layout({ children }: { children: ReactNode }) {
+  const iconStyle = { width: rem(16), height: rem(16) };
   const [opened, { toggle }] = useDisclosure();
+  const navigate = useNavigate();
+  const location = useLocation();
+
   return (
     <AppShell
       header={{ height: 60 }}
@@ -81,17 +139,44 @@ function Layout({ children }: { children: ReactNode }) {
       }}
       padding="md"
     >
-      <AppShell.Header display="flex" style={{ alignItems: "center" }}>
-        <Burger opened={opened} onClick={toggle} hiddenFrom="md" size="sm" />
+      <AppShell.Header display="flex" style={{ alignItems: "center" }} p="sm">
+        <Burger
+          mr="sm"
+          opened={opened}
+          onClick={toggle}
+          hiddenFrom="md"
+          size="sm"
+        />
       </AppShell.Header>
 
       <AppShell.Navbar p="md">
-        <Link to="/inventories/adjustments">Stock Adjustment</Link>
-        <Link to="/products">Products</Link>
-        <Link to="/purchase-orders">Purchase Order</Link>
-        <Link to="/sales">Sales</Link>
-        <Link to="/transactions">Transactions</Link>
-        <Link to="/suppliers">Suppliers</Link>
+        <ScrollArea scrollbarSize={2}>
+          {links_data.map((link, index) => (
+            <>
+              <Text mb="xs">{link.label}</Text>
+              {link.links.map((subLink) => (
+                <Button
+                  c={`${
+                    location.pathname.includes(subLink.path) ? "" : "dark"
+                  }`}
+                  onClick={() => navigate(subLink.path)}
+                  style={{ display: "flex" }}
+                  mb="xs"
+                  fullWidth
+                  variant={`${
+                    location.pathname.includes(subLink.path)
+                      ? "light"
+                      : "transparent"
+                  }`}
+                  leftSection={<subLink.icon style={iconStyle} />}
+                >
+                  {subLink.label}
+                </Button>
+              ))}
+              {index < links_data.length - 1 && <Divider mb="xs" />}
+            </>
+          ))}
+        </ScrollArea>
       </AppShell.Navbar>
 
       <AppShell.Main bg="gray.0" pos="relative">
